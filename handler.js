7334548e3cac85595ccd79b4d981ac42f5c25012
Hoiy <hoiy927@gmail.com>
2016-10-31 00:20:23 +0800

    Deploy with lambda proxy and support json/application.

--- handler.js
+++ handler.js
@@ -5,21 +5,25 @@ var cheerio = require('cheerio');
 var rfs = require('require-from-string');
 
 module.exports.main = (event, context, callback) => {
-  request(event.url, function (error, response, html) {
+  var body = JSON.parse(event.body);
+  request(body.url, function (error, response, html) {
     if (error) {
       callback(error, null);
       return;
     }
 
     try {
-      var result = rfs(event.config);
+      var result = rfs(body.config);
       var $ = cheerio.load(html);
       Object.keys(result).forEach(function(key) {
         if(typeof result[key] === 'function') {
           result[key] = result[key]($);
         }
       });
-      callback(null, result);
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result)
+      });
     } catch (e) {
       callback(e, null);
     }
