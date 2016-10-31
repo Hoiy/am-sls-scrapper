9a1b6336779c22c642f077b99f8e556070e4b393
Hoiy <hoiy927@gmail.com>
2016-10-31 23:19:29 +0800

    Support all encoding and output in UTF-8.

--- handler.js
+++ handler.js
@@ -3,26 +3,38 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var rfs = require('require-from-string');
+var iconv = require('iconv-lite');
+var charsetParser = require('charset-parser');
 
 module.exports.main = (event, context, callback) => {
   var body = JSON.parse(event.body);
-  request(body.url, function (error, response, html) {
+  request({
+    uri: body.url,
+    encoding: null,
+  }, function (error, response, binary) {
     if (error) {
       callback(error, null);
       return;
     }
 
     try {
-      var result = rfs(body.config);
+      var charset = charsetParser(response.headers['content-type'], binary, 'iso-8859-1');
+      var html = iconv.decode(binary, charset); // convert any encoding to UTF-8
       var $ = cheerio.load(html);
+
+      var result = rfs(body.config);
       Object.keys(result).forEach(function(key) {
         if(typeof result[key] === 'function') {
           result[key] = result[key]($);
         }
       });
+
       callback(null, {
         statusCode: 200,
-        body: JSON.stringify(result)
+        body: JSON.stringify(result),
+        headers: {
+          "Content-Type" : "application/json; charset=UTF-8"
+        },
       });
     } catch (e) {
       callback(e, null);
